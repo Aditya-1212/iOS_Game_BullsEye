b5a0f0bfcb3d1922b4ceda51d5b9955c6fc617ba
Aditya-1212 <adityashinde989@gmail.com>
2020-09-08 12:25:58 -0700

    First commit

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Aditya Shinde on 3/2/20.
+//  Copyright © 2020 Aditya Shinde. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+}
